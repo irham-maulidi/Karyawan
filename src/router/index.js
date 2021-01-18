9cca73cb79adb187e8bdd8e421f762b2aa8f7c72
irham maulidi <irham16101999>
2021-01-18 10:17:54 +0700

    first Commit

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+import {Menu, Home, TambahKaryawan, DetailKaryawan, EditKaryawan }from '../pages'
+
+const Stack = createStackNavigator();
+
+const Router = () => {
+    return (
+        <Stack.Navigator>
+            {/* <Stack.Screen name="Menu" component={Menu} options={{ headerShown: false}} /> */}
+            <Stack.Screen name="Home" component={Home} options={{ headerShown: false}} />
+            <Stack.Screen name="TambahKaryawan" component={TambahKaryawan} options={{title: 'Tambah Karyawan'}} />
+            <Stack.Screen name="DetailKaryawan" component={DetailKaryawan} options={{title: 'Detail Karyawan'}} />
+            <Stack.Screen name="EditKaryawan" component={EditKaryawan} options={{title: 'Edit Karyawan'}} />
+        </Stack.Navigator>
+    )
+}
+
+export default Router
